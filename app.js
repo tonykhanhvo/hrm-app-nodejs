4c8f8be7d2a3456443a9a6d26b650827fb4bdd5d
tonykhanhvo <vgkhanh1997@gmail.com>
2022-03-24 21:37:15 +0700

    Run Basic App

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // Import Core Module
-
+const path = require('path');
 // Import Third Party
-
+const express = require('express');
+const bodyParse = require('body-parser');
+const mongoose = require('mongoose');
 // Import in-App Module
 
 // Create Express App
-
+const app = express();
 // Set Template Engine
-
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 // Import Route Module
 
+// Using body-parser module
+app.use(bodyParse.urlencoded({extended: false}));
+// Set Static Folder (public folder)
+app.use(express.static(path.join(__dirname, 'public')));
 // Set Middleware
-
-// Connect Mongoose
+app.use('/', (req, res, next) => {
+    res.render('home', {
+        pageTitle: 'Home Page'
+    });
+});
+// Connect Mongoose & Set app.listen
+app.listen(3000);
